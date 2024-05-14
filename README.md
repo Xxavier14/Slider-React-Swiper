7f1484efdc0caaeb9ed579d667cd1a97835390c7
Juan P Javier <juanpablojavier14@gmail.com>
2024-05-14 16:22:28 -0300

    readme and Swiper.jsx

--- README.md
+++ README.md
@@ -2,8 +2,26 @@
 
 Slider with React, vite and Swiper.
 
-- install Swiper dependence ( also can use CDN )
+- install Swiper dependence ( also can use CDN ) -
 ``` pnpm install swipe ```
 
-- import swiper ( whitout modules )
-``` import Swiper ```
+- import swiper ( whitout modules, core version ) -
+``` import Swiper from 'swiper'```
+
+-- import swiper ( with all modules ) -
+``` import Swiper from 'swiper/bundle'```
+
+-- initialize Swiper
+const swiper = new Swiper( '#nameOfTheClass' , {
+    #optionalparameters like 
+    direction: 'vertical' | 'horizontal'
+    [examples: loop(boolean), speed(ms), spaceBetween(px)](https://swiperjs.com/swiper-api#parameters)
+
+    [moduleConfiguration:{}](https://swiperjs.com/swiper-api#modules)
+    pagination:{
+        
+    }
+}
+)
+
+- [Documentation of Swiper:](https://swiperjs.com/get-started)
--- src/Swiper.jsx
+++ src/Swiper.jsx
@@ -1,16 +1,53 @@
 // import Swiper bundle with all modules installed
-import Swiper from 'swiper/bundle';
+import Swiper from "swiper/bundle";
 
 // import styles bundle
-import 'swiper/css/bundle';
+import "swiper/css/bundle"
+import './Swiper.css'
 
 // init Swiper:
-// const swiper = new Swiper(...);
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 600,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      enabled: false,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      enabled: false,
+    },
+  });
 
-const contenido = 'Helous'
+export function ShowSlider() {
+  return (
+    <>
+      <div class="swiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide"><img src="https://cdn.pixabay.com/photo/2022/04/06/23/50/flower-7116555_1280.jpg" alt="" /></div>
+          <div class="swiper-slide"><img src="https://cdn.pixabay.com/photo/2022/12/01/13/28/leaf-7629114_1280.jpg" alt="" /></div>
+          <div class="swiper-slide"><img src="https://cdn.pixabay.com/photo/2017/08/06/20/19/orange-2595941_1280.jpg" alt="" /></div>
+        </div>
+        
+        <div class="swiper-pagination"></div>
 
-export function ShowSlider(){
-    return(
-        <h3>{contenido}</h3>
-    )
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
+        <div class="swiper-scrollbar"></div>
+      </div>
+    </>
+  );
 }
